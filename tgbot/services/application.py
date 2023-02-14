663472396663d35483186c3b092ecbd45264b79e
duke <duke.nsk@gmail.com>
2023-02-14 09:28:27 +0700

    del: return application

--- tgbot/services/application.py
+++ tgbot/services/application.py
@@ -9,7 +9,6 @@ def get_telegram_application() -> Application:
     return ApplicationBuilder().token(settings.TELEGRAM_BOT_TOKEN).build()
 
 
-def add_command_handlers(application: Application, command_handlers: dict[str, Callable]) -> Application:
+def add_command_handlers(application: Application, command_handlers: dict[str, Callable]) -> None:
     for command_name, command_handler in command_handlers.items():
         application.add_handler(CommandHandler(command_name, command_handler))
-    return application
